5ca7bec9a2b649ef8829033cc27a917ec4cb0dd1
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-06-23 17:08:16 +0200

    Global scope

---
+++ Scope/GlobalScope.js
@@ -0,0 +1,14 @@
+// 6. Global scope
+// The global scope is the outermost scope. It is accessible from any inner scope.
+
+//  the topmost scope of JavaScript file loaded using <script> tag is a global scope:
+
+<script src="myScript.js"></script>
+
+// myScript.js
+// "global" scope
+let counter = 1;
+
+// A variable declared inside the global scope is named global variable. 
+// Global variables are accessible from any scope
+
